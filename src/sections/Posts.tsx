7f1875ea1879195cc199abd385c1092ff40759ab
freetimezp <kraftukrnet@ukr.net>
2024-04-22 22:03:08 +0300

    06. js-portfolio-73-news. Posts component. Display posts from db.

--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import './posts.css';
+
+import PostItemOne from '@/components/PostItemOne';
 
 const Posts = () => {
-    return (
-        <div>
+    const router = useRouter();
 
-        </div>
+    const [items, setItems] = useState<[] | any>([]);
+
+    const getItemsData = () => {
+        fetch(`/api/postitems`)
+            .then(res => res.json())
+            .then(data => setItems(data))
+            .catch(err => console.log(err.message));
+    };
+
+    useEffect(() => {
+        getItemsData();
+    }, []);
+
+    return (
+        <section id='posts' className='posts'>
+            <div className="container" data-aos="fade-up">
+                <div className="row g-5">
+                    <div className="col-lg-4">
+
+                    </div>
+                    <div className="col-lg-8">
+                        <div className="row g-5">
+                            <div className="col-lg-4 border-start custom-border">
+                                {items && items.length > 0 && items.map((item: any, index: number) => (
+                                    <PostItemOne key={index} large={false} item={item} />
+                                ))}
+                            </div>
+                            <div className="col-lg-4"></div>
+                            <div className="col-lg-4"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     );
 }
 
